e8363787e537a65401203d3da65bedb636909286
themuzic <masta_jk@naver.com>
2022-09-05 21:04:07 +0900

    add view my tweets

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Navigation = ({ userObj }) => (
-  <nav>
-    <ul
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        marginTop: 50,
-        marginBottom: 30,
-      }}
-    >
-      <li>
-        <Link to="/">
-          <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
-        </Link>
-      </li>
-      <li>
-        <Link
-          to="/profile"
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            fontSize: 12,
-          }}
-        >
-          <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />
-          <span className="user_name">{userObj.displayName}'s Profile</span>
-        </Link>
-      </li>
-    </ul>
-  </nav>
-);
+const Navigation = ({ userObj }) => {
+  return (
+    <nav>
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 50,
+          marginBottom: 30,
+        }}
+      >
+        <li>
+          <Link to="/">
+            <FontAwesomeIcon icon={faTwitter} color={"#04AAFF"} size="2x" />
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/profile"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              fontSize: 12,
+            }}
+          >
+            <FontAwesomeIcon icon={faUser} color={"#04AAFF"} size="2x" />
+            <span className="user_name">{userObj.displayName}'s Profile</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,19 +8,45 @@ const Home = ({ userObj }) => {
   const collectionRef = collection(firebaseDB, "tweets");
   const tweetQuery = query(collectionRef, orderBy("createdAt", "desc"));
   const [tweets, setTweets] = useState([]);
+  const [isOnlyMyTweets, setIsOnlyMyTweets] = useState(false);
+
+  const handleMyTweets = (e) => {
+    setIsOnlyMyTweets(e.target.checked);
+  };
 
   useEffect(() => {
     onSnapshot(tweetQuery, (snapShot) => {
-      const tweetArr = snapShot.docs.map((doc) => ({
+      let tweetArr = snapShot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
+      if (isOnlyMyTweets) {
+        tweetArr = tweetArr.filter((tweet) => {
+          if (tweet.creatorId === userObj.uid) {
+            return tweet;
+          }
+        });
+      }
       setTweets(tweetArr);
     });
-  }, []);
+  }, [isOnlyMyTweets]);
 
   return (
     <div className="container">
+      {userObj && (
+        <>
+          <div id="my-tweet-wrap">
+            <label>
+              <input
+                type="checkbox"
+                id="my-tweets"
+                onClick={(event) => handleMyTweets(event)}
+              />
+              &nbsp;My tweets
+            </label>
+          </div>
+        </>
+      )}
       <TweetFactory userObj={userObj} collectionRef={collectionRef} />
       <div>
         {tweets.map((tw) => (
